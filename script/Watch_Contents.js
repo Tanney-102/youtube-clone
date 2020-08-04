54bd2bd72b08d7b5f34f9dfd22e763ecb8a72745
Tanney-102 <dhxodms89@gmail.com>
2020-08-05 01:46:54 +0900

    watch-layout

--- script/Watch_Contents.js
+++ script/Watch_Contents.js
@@ -38,8 +38,13 @@ function getMainVideo() {
 
 function renderVideoArea(data) {
     const videoArea = document.querySelector('#video-area');
+    const userProfile = '<i class="fas fa-user-circle"></i>' // 추후 수정
+    const created = data["created"].split('/')[0].split('-');
+    let createdDate;
 
-    console.log(data);
+    created.forEach( v => {
+        createdDate += v + '. ';
+    });
 
     videoArea.innerHTML = `
     <div id="video-player">
@@ -48,6 +53,47 @@ function renderVideoArea(data) {
             video 미지원 브라우저입니다.
         </video>
     </div>
+    <div class="video-title">${data["title"]}</div>
+        <div class="video-info">
+            <div class="video-info-head">
+                <div class="view-n-created">
+                    <span class="video-views">조회수 ${views}회</span>
+                    <span>&middot;</span>
+                    <span class="video-created">${createdDate}</span>
+                </div>
+                <div class="v-btns-container">
+                    <div id="likes-btn" class="btn-hover v-btn">
+                        <i class="fas fa-thumbs-up v-icon"></i>
+                        <div class="btn-text">${data["likes"]}</div>
+                    </div>
+                    <div id="share-btn" class="btn-hover v-btn">
+                        <i class="fas fa-share v-icon"></i>
+                        <div class="btn-text">공유</div>
+                    </div>
+                    <div id="save-btn" class="btn-hover v-btn">
+                        <i class="fas fa-plus-square v-icon"></i>
+                        <div class="btn-text">저장</div>
+                    </div>
+                </div>
+            </div>
+            <div class="video-desc">
+                <div class="author-profile">
+                    ${userProfile}
+                </div>
+                <div class="desc-container">
+                    <div class="author-info">
+                        <div>
+                            <div class="author-name">${data["author"]}</div>
+                            <div class="author-subs">구독자 100만명</div>
+                        </div>
+                        <button id="subs-btn" class="subs-btn btn-hover">구독</button>
+                    </div>
+                    <div class="desc-content">
+                        ${data["text"]}
+                    </div>
+                </div>
+            </div>
+        </div>
     `;
 
     setVideoWidth();
