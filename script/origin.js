71294c04cef895fffed1c224b33e128aae329aef
Tanney-102 <dhxodms89@gmail.com>
2020-08-24 02:30:41 +0900

    url

--- script/origin.js
+++ script/origin.js
@@ -1,3 +1,3 @@
 const origin_home = 'http://localhost:4000/'
 const origin_net = 'https://tjyoutube-clone.netlify.app/';
-const origin_server = 'http://youtubeclone1535.herokuapp.com/'
+const origin_server = 'https://youtubeclone1535.herokuapp.com/'
